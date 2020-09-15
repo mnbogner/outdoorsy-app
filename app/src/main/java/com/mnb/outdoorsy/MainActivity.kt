d41afe102623848a9a24fb58efb45a724c11830c
mnbogner <mnbogner@eds.org>
2020-09-14 17:19:37 -0700

    built ui components based on examination of provided images (unable to open .sketch file)

--- app/src/main/java/com/mnb/outdoorsy/MainActivity.kt
+++ app/src/main/java/com/mnb/outdoorsy/MainActivity.kt
@@ -4,8 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        window.statusBarColor = resources.getColor(R.color.darkerGreen, theme)
+        supportActionBar?.hide()
+
         setContentView(R.layout.activity_main)
     }
+
 }
